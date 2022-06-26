8f1d16442e38608b06b50cbb36b170e38bd47730
Alex-Vis <usenkoshurik@mail.ru>
2022-06-26 16:26:21 +0300

    Add GifService tests

--- src/test/java/com/alex/vis/service/GiphyGifServiceImplTest.java
+++ src/test/java/com/alex/vis/service/GiphyGifServiceImplTest.java
@@ -1,4 +1,41 @@
 package com.alex.vis.service;
 
+import com.alex.vis.client.FeignGiphyClient;
+import com.alex.vis.service.service_impl.GiphyGifServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ComponentScan("alex.vis")
 public class GiphyGifServiceImplTest {
+
+    @Autowired
+    private GiphyGifServiceImpl gifService;
+    @MockBean
+    private FeignGiphyClient gifClient;
+
+    @Test
+    public void whenPositiveChanges() {
+        ResponseEntity<Map> testEntity = new ResponseEntity<>(new HashMap(), HttpStatus.OK);
+        Mockito.when(gifClient.getRandomGif(anyString(), anyString()))
+                .thenReturn(testEntity);
+        ResponseEntity<Map> result = gifService.getGif("control_test_word");
+        assertEquals("control_test_word", result.getBody().get("compareResult"));
+    }
+
 }
